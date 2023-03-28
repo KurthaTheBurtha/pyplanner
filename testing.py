06cbb30d33c9095a003ede13cb7ff255f2d9df0f
KurthaTheBurtha <kschimmel@waprep.org>
2023-03-27 20:31:07 -0700

    Update testing.py
    poop

--- testing.py
+++ testing.py
@@ -1,16 +1,28 @@
-import datetime
-x = datetime.datetime.now()
-today = x.strftime("%m/%d/%Y")
-month = today[0:today.index("/")]
-second = today.find("/",3)
-day = today[today.index("/")+1:second]
-year = today[second+1:len(today)]
-print(today)
-print(month)
-print(day)
-print(year)
-year = datetime.datetime.now().strftime("%Y")
+# import datetime
+# x = datetime.datetime.now()
+# today = x.strftime("%m/%d/%Y")
+# month = today[0:today.index("/")]
+# second = today.find("/",3)
+# day = today[today.index("/")+1:second]
+# year = today[second+1:len(today)]
+# print(today)
+# print(month)
+# print(day)
+# print(year)
+# year = datetime.datetime.now().strftime("%Y")
 #print(day)
+index=0
+k=3
+a = []
+for i in range(1,7+1):
+    a.append(i)
+while k>index:
+        if a[index]<a[len(a)-index-1]:
+            a[index],a[len(a)-index-1] = a[len(a)-index-1],a[index]
+        index+=1
+        if index>=k:
+            break
+print(a)
 
 class Date:
 
